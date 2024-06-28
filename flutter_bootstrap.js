43f1a87121140ceab658492433cf78f95d757096
tachen9981 <tachen9981@gmail.com>
2024-06-28 08:36:42 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "529270289"
+    serviceWorkerVersion: "2679743707"
   }
 });
